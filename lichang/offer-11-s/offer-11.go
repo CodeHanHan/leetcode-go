7b016c4221add201f1e7a4b8d775141079941b99
yangchnet <1048887414@qq.com>
2021-09-12 16:48:02 +0800

    feat: add offer11

--- lichang/offer-11-s/offer-11.go
+++ lichang/offer-11-s/offer-11.go
@@ -1,5 +1,18 @@
 package offer_11_s
 
 func minArray(numbers []int) int {
-	panic("not implement")
+	start := 0
+	end := len(numbers) - 1
+
+	for start < end {
+		mid := start + (end-start)/2
+		if numbers[mid] < numbers[end] {
+			end = mid
+		} else if numbers[mid] > numbers[end] {
+			start = mid + 1
+		} else {
+			end = end - 1
+		}
+	}
+	return numbers[start]
 }
