d0dd7f547003a8c29cacbd60521f62f1acf7f084
yangchnet <1048887414@qq.com>
2021-10-09 11:12:54 +0800

    feat: topic-20

---
+++ yangchnet/base/Stack/stack_test.go
@@ -0,0 +1,29 @@
+package stack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_stack(t *testing.T) {
+	s := NewStack()
+	s.Push("1")
+	fmt.Println(s)
+
+	s.Push("2")
+	s.Push("3")
+	fmt.Println(s)
+
+	s.Pop()
+	fmt.Println(s)
+	s.Pop()
+	_, ok := s.Pop()
+	if !ok {
+		fmt.Println("stack is empty")
+	}
+
+	_, ok = s.Pop()
+	if !ok {
+		fmt.Println("stack is empty")
+	}
+}
