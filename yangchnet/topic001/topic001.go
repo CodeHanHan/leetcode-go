d4dd448cb47f2f6d5c800e24df583100f332cf17
yangchnet <1048887414@qq.com>
2021-11-08 09:45:45 +0800

    feat: topic001

---
+++ yangchnet/topic001/topic001.go
@@ -0,0 +1,27 @@
+package topic001
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+func twoSum_1(nums []int, target int) []int {
+	wanted := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		if pre, ok := wanted[nums[i]]; ok {
+			return []int{pre, i}
+		}
+
+		wanted[target-nums[i]] = i
+	}
+
+	return []int{}
+}
