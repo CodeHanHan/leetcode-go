93486be7fd96b823d090e3d9425c8bae805c2434
HT-CHEN520 <252593241@qq.com>
2021-11-13 16:42:12 +0800

    feat:lc137

--- HT-CHEN520/lc137/lc137.go
+++ HT-CHEN520/lc137/lc137.go
@@ -14,3 +14,16 @@ func singleNumber(nums []int) int {
 	}
 	return 0
 }
+
+func singleNumber_1(nums []int) int {
+	bitNum, res := int32(0), int32(0)
+	for i := 0; i < 32; i++ {
+		bitNum = int32(0)
+
+		for _, val := range nums {
+			bitNum += (int32(val) >> i) & 1
+		}
+		res |= (bitNum % 3) << i
+	}
+	return int(res)
+}
