3598561c4c6df2b10864f2142b1b3c39b6249617
HT-CHEN520 <252593241@qq.com>
2021-12-29 15:26:29 +0800

    fix: lc268

--- HT-CHEN520/lc268/lc268_test.go
+++ HT-CHEN520/lc268/lc268_test.go
@@ -43,4 +43,6 @@ func Test_missingNumber_2(t *testing.T) {
 	require.Equal(t, 1, missingNumber([]int{0}))
 
 	require.Equal(t, 2, missingNumber([]int{3, 0, 1, 4, 5}))
+
+	require.Equal(t, 4, missingNumber([]int{3, 2, 0, 1, 5}))
 }
