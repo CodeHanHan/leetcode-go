11826ad28456896a454b7b5392971da1b4385ded
yangchnet <1048887414@qq.com>
2022-02-23 22:02:56 +0800

    feat(goden): goden0101

---
+++ yangchnet/goden/goden0101/goden0101.go
@@ -0,0 +1,15 @@
+package goden0101
+
+func isUnique(astr string) bool {
+	var bin int64 = 0
+	for _, b := range astr {
+		dt := b - 'a'
+		if bin&(1<<dt) > 0 {
+			return false
+		}
+
+		bin = bin | (1 << dt)
+	}
+
+	return true
+}
